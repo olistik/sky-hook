2b935d78f5d4644aa8414e73725527100fe14bea
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2014-06-12 16:46:52 +0200

    genesis

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-sky-hook
-========
+Sky-Hook
+==========
 
-Emulates a browser (via CasperJS/PhantomJS) in order to grab some information provided by the Nexive platform.
+This tool emulates a browser (via CasperJS/PhantomJS) in order to grab some information provided by the [Nexive platform](https://www.sistemacompleto.it/Senders/Ricerche/TrackAndTrace.aspx).
+
+## Requirements
+
+Tested with the following versions:
+
+```
+$ node -v
+v0.10.28
+$ npm -v
+1.4.13
+$ casperjs --version
+1.1.0-beta3
+```
+
+## Install
+
+```
+$ npm install --save moment
+```
+
+## Usage
+
+Create a file ```credentials.json```:
+
+```json
+{
+  "username": "my@account.com",
+  "password": "myPassword"
+}
+```
+
+```
+$ casperjs nexive.js
+```
+
+The resulting file will be stored in the working directory and named with a date timestamp:
+
+```
+$ cat $(ls nexive-*.json | sort | tail -n 1)
+```
---
+++ nexive.js
@@ -0,0 +1,139 @@
+var moment = require('moment');
+var casper = require('casper').create();
+var fs = require('fs');
+
+var credentials = require('credentials.json');
+
+var startingUrl = 'https://www.sistemacompleto.it/Senders/Ricerche/TrackAndTrace.aspx';
+
+// Step 1
+var loginStep = function() {
+  this.fillSelectors('form[name="aspnetForm"]', {
+    'input[name="ctl00$contentPlaceHolder$usernameIn"]': credentials.username,
+    'input[name="ctl00$contentPlaceHolder$passwordIn"]': credentials.password
+  }, false);
+
+  this.click('#ctl00_contentPlaceHolder_label_btnAccedi');
+};
+
+// Step 2
+var checkAdvancedSearchButton = function() {
+  return this.evaluate(function() {
+    return $('#ctl00_cphMainContext_hlkTrackAndTrace').length == 1;
+  });
+}
+
+var clickAdvancedSearchButton = function() {
+  this.click('#ctl00_cphMainContext_hlkTrackAndTrace');
+}
+
+// global vars
+var stepCurrentPageIndex = 1;
+var shipmentStatuses = [];
+
+var fetchCurrentPageIndex = function() {
+  return this.evaluate(function() {
+    return parseInt($('.dxpCurrentPageNumber').html().slice(1, -1), 10);
+  });
+}
+
+// Step 3 (recursive)
+var checkCurrentPage = function() {
+  return fetchCurrentPageIndex.bind(this)() == stepCurrentPageIndex;
+}
+
+var parseCurrentPage = function() {
+  this.echo('Current page index: ' + fetchCurrentPageIndex.bind(this)());
+  var pageShipmentStatuses = this.evaluate(function() {
+    return $('.dxgvTable .dxgvDataRow').map(function(index, element) {
+      var indexMapping = {
+        barcode: 1, // BarCode
+        firstName: 2, // Nome
+        lastName: 3, // Cognome
+        city: 4, // Comune
+        province: 5, // Prov
+        lastUpdatedAt: 6, // Data Ultimo Stato
+        state: 7, // Stato
+        reason: 8, // Motivo
+        cod: 9, // COD
+        units: 11, // Colli
+        estimatedDeliveryDate: 12, // Data Prevista Consegna
+        fileId: 13, // Id File
+        createdAt: 14, // Data Creazione
+        customerReference: 15, // Rif. Cliente
+        priceFacility: 16 // Centro di Costo
+      };
+      var data = {};
+      for (var key in indexMapping) {
+        data[key] = $(element).children().eq(indexMapping[key]).html();
+        if (data[key] == '&nbsp;') {
+          data[key] = '';
+        }
+      }
+
+      // size: 10, // Taglia
+      data.size = $(element).children().eq(10).children('span').html();
+
+      return data;
+    }).get();
+  });
+  shipmentStatuses = shipmentStatuses.concat(pageShipmentStatuses);
+}
+
+var isLastPage = function() {
+  return this.evaluate(function() {
+    return $('.dxWeb_pNextDisabled').length == 1;
+  });
+}
+
+var advanceToNextPage = function() {
+  this.evaluate(function() {
+    var nextPaginationLink = $('.dxpCurrentPageNumber').nextAll('.dxpPageNumber').eq(0);
+    nextPaginationLink.click();
+  });
+  ++stepCurrentPageIndex;
+  this.waitFor(
+    checkCurrentPage.bind(this),
+    parseCurrentPageAndAdvanceUnlessLastPage.bind(this)
+  );
+}
+
+var parseCurrentPageAndAdvanceUnlessLastPage = function() {
+  parseCurrentPage.bind(this)();
+  if (!isLastPage.bind(this)()) {
+    advanceToNextPage.bind(this)();
+  }
+}
+
+// Execution flow
+casper.start(startingUrl, loginStep.bind(casper));
+
+casper.waitFor(
+  checkAdvancedSearchButton.bind(casper),
+  clickAdvancedSearchButton.bind(casper)
+);
+
+casper.waitFor(
+  checkCurrentPage.bind(casper),
+  parseCurrentPageAndAdvanceUnlessLastPage.bind(casper)
+);
+
+// debugging only
+var printResults = function() {
+  var barcodes = shipmentStatuses.map(function(shipmentStatus) {
+    return shipmentStatus.barcode;
+  }).join(', ');
+  this.echo('Extracted ' + shipmentStatuses.length + ' records: [' + barcodes + ']');
+}
+
+var dumpResultsToFile = function() {
+  var outputFilename = moment().format('[nexive-]YYYYMMDDhhmmss[.json]');
+  fs.write(outputFilename, JSON.stringify(shipmentStatuses), 'w');
+  this.echo('Results stored in: ' + outputFilename);
+}
+
+// Ending code
+casper.run(function() {
+  dumpResultsToFile.bind(this)();
+  this.exit();
+});
